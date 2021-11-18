a275896be1baefe9b76f1d9e286d904dc7fe2e48
Emilia Preuss <empre@onet.pl>
2021-11-18 22:59:10 +0100

    Fixed bugs with translations and deleted 'new' flag

--- webshop/modules/psaddonsconnect/translations/pl.php
+++ webshop/modules/psaddonsconnect/translations/pl.php
@@ -3,7 +3,7 @@
 global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{psaddonsconnect}prestashop>psaddonsconnect_59c29985e3ebd042bef35631307d47a2'] = 'Aktualizacje i porady';
-$_MODULE['<{psaddonsconnect}prestashop>psaddonsconnect_475068f091f2d33ff771152fcc008714'] = '';
+$_MODULE['<{psaddonsconnect}prestashop>psaddonsconnect_33dd2b331a3344a8158e4dd15f1cd6f3'] = 'Stwórz bezpieczne i funkcjonalne aktualizacje wszystkich modułów w sklepie ';
 $_MODULE['<{psaddonsconnect}prestashop>psaddonsconnect_bb8956c67b82c7444a80c6b2433dd8b4'] = 'Czy na pewno chcesz odinstalować ten moduł?';
 $_MODULE['<{psaddonsconnect}prestashop>psaddonsconnect_f87b9438e8de65a83e7a3ef56a9d23fb'] = 'Podczas instalacji wystąpił błąd. Skontaktuj się z nami przez witrynę Addons';
 $_MODULE['<{psaddonsconnect}prestashop>psaddonsconnect_78d320af42aca685d1fcd1113f09939e'] = 'Podczas odinstalowywania wystąpił błąd. Skontaktuj się z nami przez witrynę Addons';
@@ -19,15 +19,12 @@ $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_dc647eb65e6711e1553752
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_878530871f0db73f004f5bd6591eeb76'] = 'Zapamiętaj mnie';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_2283584ec794bba6bb3a045853c87a61'] = 'ZACZYNAJMY!';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_01a569ddc6cf67ddec2a683f0a5f5956'] = 'Zapomniałeś hasła?';
-$_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_78f1d8c0b05e45e2fd5e2a9549e87589'] = 'POLACZ Z PLATFORMA PRESTASHOP';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_c4ab50c3866c157bd46ed930ab305eb8'] = 'Porada na dziś';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_754498b2927bb601e4072f1d7385b984'] = 'Zobacz pełny wybór';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_bc74efcae6217e0f80935d5307ddfea0'] = 'Praktyczne linki';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_7f3ef03ec9e8db5866a9f02350cfe746'] = 'Moduły do';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_43bcc86750a173fe79da9ed9fef3e689'] = 'przyciąganiu ruchu na stronę';
-$_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_7f3ef03ec9e8db5866a9f02350cfe746'] = 'Moduły do';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_e397072999e5885f96e030977f733b3d'] = 'optymalizacji konwersji';
-$_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_7f3ef03ec9e8db5866a9f02350cfe746'] = 'Moduły do';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_670fdaa07cd73dd95da8f0bfd1ce0b51'] = 'zwiększania średniej wartości koszyka';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_dd00e3e6f9adb69272dfad842320bd4e'] = 'Wybór modułów zalecanych dla';
 $_MODULE['<{psaddonsconnect}prestashop>dashboard_zone_one_9494a44a23e3dbca3a5c8af826dd3c9c'] = 'mojego sektora działalności';
